321ed9ffd102941c736731b8e7bae811820d267d
SALLEYRON Julien <julien.salleyron@gmail.com>
2017-08-18 01:20:02 +0200

    fix problem with query params

--- forward/fwd_websocket_test.go
+++ forward/fwd_websocket_test.go
@@ -95,7 +95,6 @@ func (s *FwdSuite) TestWebsocketServerWithoutCheckOrigin(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(resp, Equals, "ok")
 }
-
 func (s *FwdSuite) TestWebsocketRequestWithOrigin(c *C) {
 	f, err := New()
 	c.Assert(err, IsNil)
@@ -143,6 +142,52 @@ func (s *FwdSuite) TestWebsocketRequestWithOrigin(c *C) {
 	resp, err := newWebsocketRequest(
 		withServer(proxyAddr),
 		withPath("/ws"),
+		withData("ok"),
+	).send()
+
+	c.Assert(err, IsNil)
+	c.Assert(resp, Equals, "ok")
+}
+
+func (s *FwdSuite) TestWebsocketRequestWithQueryParams(c *C) {
+	f, err := New()
+	c.Assert(err, IsNil)
+
+	upgrader := gorillawebsocket.Upgrader{}
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			return
+		}
+		defer conn.Close()
+		c.Assert(r.URL.Query().Get("query"), Equals, "test")
+		for {
+			mt, message, err := conn.ReadMessage()
+			if err != nil {
+				break
+			}
+			err = conn.WriteMessage(mt, message)
+			if err != nil {
+				break
+			}
+		}
+	}))
+	defer srv.Close()
+
+	proxy := testutils.NewHandler(func(w http.ResponseWriter, req *http.Request) {
+		path := req.URL.Path // keep the original path
+		// Set new backend URL
+		req.URL = testutils.ParseURI(srv.URL)
+		req.URL.Path = path
+		f.ServeHTTP(w, req)
+	})
+	defer proxy.Close()
+
+	proxyAddr := proxy.Listener.Addr().String()
+
+	resp, err := newWebsocketRequest(
+		withServer(proxyAddr),
+		withPath("/ws?query=test"),
 		withData("ok"),
 	).send()
 
@@ -253,6 +298,7 @@ func (s *FwdSuite) TestForwardsWebsocketTraffic(c *C) {
 		// Set new backend URL
 		req.URL = testutils.ParseURI(srv.URL)
 		req.URL.Path = path
+
 		f.ServeHTTP(w, req)
 	})
 	defer proxy.Close()
